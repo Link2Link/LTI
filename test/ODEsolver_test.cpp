9b833f5c33ad98863488d22fbf79c882879b31dc
WANG CHENFEI <barrycf.wang@gmail.com>
2023-07-09 11:12:03 +0800

    Create ODEsolver_test.cpp

---
+++ test/ODEsolver_test.cpp
@@ -0,0 +1,49 @@
+#include "doctest/doctest.h"
+#include <iostream>
+#include "LTI/ODEsolver.hpp"
+
+
+TEST_CASE("ODE Test : First Order Integrator") {
+	using namespace LTI;
+	using namespace std;
+	State<1> y;
+	State<1> u;
+	double t = 0;
+	y << 0;
+	u << 0;
+	auto handle = Model::FirstOrderIntegrator;
+
+	double v = EIGEN_PI;
+
+	for (; t<10;)
+	{
+		u(0) = v;
+		y = RK4<1, 1>(y, u, t, 1E-3, handle);
+		t += 1E-3;
+	}
+	auto trans = v * t;
+	CHECK(y(0) == doctest::Approx(trans).epsilon(1E-6));
+}
+
+TEST_CASE("ODE Test : Second Order Integrator") {
+	using namespace LTI;
+	using namespace std;
+	State<2> y;
+	State<1> u;
+	double t = 0;
+	y << 0, 0;
+	u << 0;
+	auto handle = Model::SecondOrderIntegrator;
+
+	double a = EIGEN_PI;
+
+	for (; t<10;)
+	{
+		u(0) = a;
+		y = RK4_doublestep<2, 1>(y, u, t, 1E-3, handle);
+		t += 1E-3;
+	}
+	auto trans = 0.5 * a * pow(t, 2);
+	CHECK(y(0) == doctest::Approx(trans).epsilon(1E-6));
+	cout << trans << endl;
+}
